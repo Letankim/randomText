27aed1acc3e04c788dc4a96542cca9caf2386160
Letankim <89767092+Letankim@users.noreply.github.com>
2023-03-11 17:06:29 +0700

    update

--- app.js
+++ app.js
@@ -1,5 +1,10 @@
 var input = document.querySelector('input'),
-    span = document.querySelector('span');
+    span = document.querySelector('span'),
+    titleInput = document.querySelector('.title');
+//  xử lí handle khi focus vào input
+titleInput.onclick = () => {
+    input.focus();
+}
 input.onfocus = function() {
     span.classList.add("active");
 }
@@ -8,25 +13,59 @@ input.onblur = function() {
         span.classList.remove("active");
     }
 }
-var boxSoluong = document.querySelector('.btn button');
+// tạo array chứa thông tin
+var result = new Array();
+var boxSoluong = document.querySelector('.btn');
 boxSoluong.addEventListener('click', function() {
-    var result = new Array();
-    var inputValue = document.querySelector('input').value;
-    if(inputValue == "") {
-        alert("Please enter your words you want");
+    handleRandom();
+});
+input.addEventListener('keydown', function(e) {
+    if(e.keyCode == 13) {
+        handleRandom();
+    }
+})
+
+let clear = document.querySelector(".btn_clear");
+if(clear) {
+    clear.onclick = () => {
+        let text = document.querySelector(".random");
+        text.innerHTML = "";
+    }
+}
+
+let hre = document.querySelector('.copy'),
+    copyDone = document.querySelector('.done');
+if(hre) {
+    hre.addEventListener('click', function(e) {
+        e.preventDefault();
+        let text = document.querySelector(".random").innerText;
+        let input = document.createElement('input');
+        document.body.appendChild(input);
+        input.value = text;
+        input.select();
+        document.execCommand('copy'); 
+        input.remove();
+        copyDone.classList.add('active');
+        setTimeout(function() {
+            copyDone.classList.remove('active');
+        }, 5000)
+    });
+}
+
+function handleRandom() {
+    result = [];
+    var inputValue = input.value;
+    if(inputValue == "" || inputValue <= 0 || isNaN(inputValue)) {
+        alert("Trường này phải là 1 số dương!!");
         return 0;
     }
-    var index = 0;
-    var laplai = setInterval(() => {
-        var inputValue = document.querySelector('input').value;
-        if(index == inputValue) {
-            clearInterval(laplai);
+    for(var i = 1; i <= inputValue; i++) {
+        random(Math.floor(Math.random() * 10) + 3);
+        if(i == inputValue) {
             result = result.join(' ');
             document.querySelector(".random").innerHTML = result;
         }
-        random(Math.floor(Math.random() * 10) + 3);
-        index++;
-    }, 0);
+    }
     function random(soluong) {
         let chuhoa = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
         let chuthuong = "abcdefghijklmnopqrstuvwxyz";
@@ -48,23 +87,25 @@ boxSoluong.addEventListener('click', function() {
         arr = arr.join('');
         result.push(arr);
     }
-})
-
-let hre = document.querySelector('.copy'),
-    copyDone = document.querySelector('.done');
-if(hre) {
-    hre.addEventListener('click', function(e) {
-        e.preventDefault();
-        let text = document.querySelector(".random").innerText;
-        let input = document.createElement('input');
-        document.body.appendChild(input) ;
-        input.value = text;
-        input.select();
-        document.execCommand('copy'); 
-        input.remove();
-        copyDone.classList.add('active');
-        setTimeout(function() {
-            copyDone.classList.remove('active');
-        }, 5000)
-    });
 }
+
+// dard mode
+let btn_change = document.querySelector(".change_btn"),
+    box_dark = document.querySelector(".box_dark_mode"),
+    body_web = document.querySelector("body");
+//  check isDark
+let getDark = JSON.parse(localStorage.getItem("isDark"));
+if(getDark == true) {
+    box_dark.classList.toggle("active");
+    body_web.classList.toggle("active");
+}
+btn_change.onclick = function() {
+    box_dark.classList.toggle("active");
+    body_web.classList.toggle("active");
+    let isDark = false;
+    if(box_dark.classList.contains("active")){
+        isDark = true;
+    }
+    localStorage.setItem("isDark", JSON.stringify(isDark));
+}
+
